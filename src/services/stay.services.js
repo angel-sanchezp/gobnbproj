5de8d895fe559b7e110d4b4a7c97840aa5482276
LiorAki <lior21231@gmail.com>
2022-01-19 09:40:48 +0200

    fixes

--- src/services/stay.services.js
+++ src/services/stay.services.js
@@ -1,11 +1,16 @@
 import { utilService } from "./utils.service.js";
 import { storageService } from "./storage.service.js"
 
-// export const stayService = {
-    
-// }
-
 const STAY_KEY = 'stayDB';
+export const stayService = {
+    query,
+}
+
+function query(){
+    _createStays();
+}
+
+_createStays()
 
 // In progress : copying information
 function _createStays() {
@@ -13,14 +18,30 @@ function _createStays() {
     if(!stays || !stays.length) {
         stays = [];
 
+        // Tokyo
+        stays.push(_createStay("200001", "Shinjuku place", "Apartment", ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'], 50, 'Information about the place', 2, 2, 1, 1, ['6.jpg'], ['Wifi', 'Dryer' , 'Kitchen', 'Parking'], 'Japan', 'JPN', 'Shinjuku', 35.698796, 139.708853));
+        stays.push(_createStay("200002", "Asakusa place", "House", ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'], 300, 'Information about the place', 5, 4, 3, 2, ['6.jpg'], ['Wifi', 'Wash disher' , 'Kitchen', 'Parking', 'Working space'], 'Japan', 'JPN', 'Asakusa', 35.719782, 139.793300));
+        stays.push(_createStay("200003", "Shibuya place", "Apartment", ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'], 120, 'Information about the place', 3, 3, 2, 1, ['6.jpg'], ['TV', 'Dryer' , 'Kitchen', 'Parking', 'AC'], 'Japan', 'JPN', 'Shibuya', 35.664252, 139.702251));
+        console.log(stays);
+        // Paris
 
+        // London
+
+        // New Zealand
+
+        // Mexico
+
+        // Others
 
     }
+
+    _saveStaysToStorage(stays);
+    return stays;
 }
 
 
 // Dynamic function
-function _createStay(id, name, type, imgUrls, price, summary, capacity, bedrooms, beds, bathrooms, sleep, amenities, host, country, countryCode, address, lat, lng, reviews) {
+function _createStay(id, name, type, imgUrls, price, summary, capacity, bedrooms, beds, bathrooms, sleep, amenities, country, countryCode, address, lat, lng) {
     return {
         id,
         name,
@@ -34,11 +55,7 @@ function _createStay(id, name, type, imgUrls, price, summary, capacity, bedrooms
         bathrooms,
         sleep,
         amenities,
-        host: {
-            id:host.id,
-            fullName: host.fullName,
-            imgUrl: host.imgUrl,
-        },
+        
         location:{
             country,
             countryCode,
@@ -46,7 +63,10 @@ function _createStay(id, name, type, imgUrls, price, summary, capacity, bedrooms
             lat,
             lng,
         },
-        reviews
+        // reviews:{
+        //     user1,
+        //     user2
+        // },
     }
 }
 
