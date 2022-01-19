089fb697a1a901cce2d8b2233c393ebf72cde004
Liat <liat100200@gmail.com>
2022-01-19 12:03:56 +0200

    header

---
+++ src/pages/Explore.jsx
@@ -0,0 +1,20 @@
+// import React, { useEffect } from 'react'
+// import { connect } from 'react-redux'
+
+// import { loadStays } from '../store/stay/stay.actions.js'
+
+
+// function _Explore({ loadStays }) {
+
+//     useEffect(() => {
+//         loadStays()
+//     }, [])
+
+    
+//     const onSelectStay = (stayId) => {
+//         console.log(` ${stay.vendor} to Cart`)
+//         addToCart(car)
+//         showSuccessMsg('Added to Cart')
+//     }
+
+// }
