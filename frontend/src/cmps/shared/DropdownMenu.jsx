f8efd77608add529dda3473cd902875b92898537
LiorAki <lior21231@gmail.com>
2022-01-26 10:02:16 +0200

    test

--- frontend/src/cmps/shared/DropdownMenu.jsx
+++ frontend/src/cmps/shared/DropdownMenu.jsx
@@ -1,79 +1,87 @@
 import React, { useRef } from "react";
-import { connect } from 'react-redux'
-import { withRouter } from "react-router-dom"
-import { NavLink } from 'react-router-dom'
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // import { useDetectOutsideClick } from "./useDetectOutsideClick.jsx";
-import UserAvatar from '../../assets/user-icon.png';
-import { ReactComponent as Burger } from '../../assets/svg/burger.svg'
+import UserAvatar from "../../assets/user-icon.png";
+import { ReactComponent as Burger } from "../../assets/svg/burger.svg";
 // import { ReactComponent as UserAvatar } from '../../assets/svg/user.svg'
-import { logout } from '../../store/user/user.actions'
+import { logout } from "../../store/user/user.actions";
 import { render } from "sass";
 
+export class DropdownMenu extends React.Component {
+  state = {
+    isActive: false,
+  };
 
-export  class DropdownMenu extends React.Component {
-    state = {
-      isActive: false
-    }
+  openLoginModal = () => {
+    document.querySelector(".user-modal").classList.remove("hidden");
+    document.querySelector(".dark-screen").classList.remove("hidden");
+  };
 
-    openLoginModal = () => {
-        document.querySelector(".user-modal").classList.remove("hidden")
-        document.querySelector(".dark-screen").classList.remove("hidden");
-    }
+  onHandleClick = (target) => {
+    console.log("hi");
+    // this.history.push(`/${target}`)
+    // window.location.href(`/${target}`);
+  };
 
-    onHandleClick = (target) => {
-      console.log('hi')
-      this.history.push(`/${target}`)
+  onHandelNav = () => {
+    if (isActive) {
+      window.addEventListener("click");
+      document.querySelector("menu").classList.add("active");
+      document.querySelector("menu").classList.remove("inactive");
+    } else {
+      window.removeEventListener("click");
+      document.querySelector("menu").classList.remove("active");
+      document.querySelector("menu").classList.add("inactive");
     }
-    // const isActive = false
-     onHandelNav = () => {
-      if(isActive){
-        window.addEventListener("click");
-        document.querySelector('menu').classList.add('active');
-        document.querySelector('menu').classList.remove('inactive');
-      } else {
-        window.removeEventListener("click");
-        document.querySelector('menu').classList.remove('active');
-        document.querySelector('menu').classList.add('inactive');
-      }
-      isActive = !isActive;
-    }
+    isActive = !isActive;
+  };
 
-  
-    render() {
-  return (
-    <div className="container">
-      <div className="menu-container">
-        <button onClick={onHandelNav} className="menu-trigger">
-          <span><Burger/></span>
-          <img className="menu-img"
-            src={UserAvatar}
-            alt="User avatar"
-          />
-        </button>
-        <nav
-          className={`menu ${isActive ? "active" : "inactive"}`}
-        >
-          <ul>
-            <li>
-            <button className="menu-opt"onClick={()=>openLoginModal()}>Login / Sign up</button>
-            </li>
-            <li>
-              <button className="menu-opt" href="#">Messages</button>
-            </li>
-            <li>
-              <button className="menu-opt" onClick={()=>onHandleClick('trips')}>Trips</button>
-            </li>
-            <li>
-              <button className="menu-opt" href="#">Dashboard</button>
-            </li>
-            <li>
-              <button className="menu-opt hidden" href="#">Log Out</button>
-            </li>
-          </ul>
-        </nav>
+  render() {
+    return (
+      <div className="container">
+        <div className="menu-container">
+          <button onClick={onHandelNav} className="menu-trigger">
+            <span>
+              <Burger />
+            </span>
+            <img className="menu-img" src={UserAvatar} alt="User avatar" />
+          </button>
+          <nav className={`menu ${isActive ? "active" : "inactive"}`}>
+            <ul>
+              <li>
+                <button className="menu-opt" 
+                onClick={() => openLoginModal()}>
+                  Login / Sign up
+                </button>
+              </li>
+              <li>
+                <button className="menu-opt" href="#">
+                  Messages
+                </button>
+              </li>
+              <li>
+                <button
+                  className="menu-opt"
+                  onClick={() => onHandleClick("trips")}>
+                  Trips
+                </button>
+              </li>
+              <li>
+                <button className="menu-opt" href="#">
+                  Dashboard
+                </button>
+              </li>
+              <li>
+                <button className="menu-opt hidden" href="#">
+                  Log Out
+                </button>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
-  );
-    }
+    );
+  }
 }
-
